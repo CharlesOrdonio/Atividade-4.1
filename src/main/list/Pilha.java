a8c6fc5471d885a55b8ccd77187c056a50b65ddd
CharlesOrdonio <119359066+CharlesOrdonio@users.noreply.github.com>
2023-11-16 17:45:59 -0300

    Update fila

--- src/main/list/Pilha.java
+++ src/main/list/Pilha.java
@@ -1,10 +1,28 @@
 package list;
 
-public interface Pilha{
+import java.util.ArrayList;
 
-    public boolean isEmpty();
+public interface Pilha{
+    ArrayList Pilha = new ArrayList();
+
+    public boolean isEmpty(){
+        if(Pilha.lenght == null){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
      
-     public void push(int item);
+    public void push(int item){
+        Pilha.add(item);
+    }
   
-     public int pop();
+    public int pop(){
+        int indice = Pilha.size() - 1;
+        int valor = indice;
+        Pilha.remove(indice);
+        return valor;
+     }
 }
