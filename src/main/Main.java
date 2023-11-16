3e0ecc578bb0ac8c0b52e6148cce75346b4bdc2d
CharlesOrdonio <119359066+CharlesOrdonio@users.noreply.github.com>
2023-11-16 17:47:34 -0300

    Update Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 import list.EstruturaElementar;
+import list.FilaComPilha;
 
 public class Main {
     public static void main(String[] args) {
-    
+        public class pilhaCharles implements pilha{
+
+        }
+        public class filaCharles implements fila{
+
+        }
+        public class fcpCharles implements FilaComPilha{
+            
+        }
     }
 }
